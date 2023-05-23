fca31a66f0104bd5fe37b07ddd1b70c25ec3cedb
konero <konero@users.noreply.github.com>
2023-05-23 21:39:27 +0100

    clang-format (#3)
    Co-authored-by: konero <19820721+konero@users.noreply.github.com>

--- iconManager.cpp
+++ iconManager.cpp
@@ -3,40 +3,41 @@
 
 IconManager::IconManager() {}
 
-void IconManager::loadIconsFromResource(const QString& resourcePath) {
-    QDirIterator it(resourcePath, QStringList() << "*.svg" << "*.png", QDir::Files, QDirIterator::Subdirectories);
+void IconManager::loadIconsFromResource(const QString &resourcePath) {
+  QDirIterator it(resourcePath,
+                  QStringList() << "*.svg"
+                                << "*.png",
+                  QDir::Files, QDirIterator::Subdirectories);
 
-    while (it.hasNext()) {
-        it.next();
-        QIcon icon(it.filePath());
-        QString iconName = it.fileInfo().baseName();
-        iconMap.insert(iconName, icon);
-        iconPaths.insert(iconName, it.filePath());
-    }
+  while (it.hasNext()) {
+    it.next();
+    QIcon icon(it.filePath());
+    QString iconName = it.fileInfo().baseName();
+    iconMap.insert(iconName, icon);
+    iconPaths.insert(iconName, it.filePath());
+  }
 }
 
-const QMap<QString, QIcon>& IconManager::getIconMap() const {
-    return iconMap;
+const QMap<QString, QIcon> &IconManager::getIconMap() const { return iconMap; }
+
+QIcon IconManager::getIcon(const QString &filename) const {
+  return iconMap.value(filename);
 }
 
-QIcon IconManager::getIcon(const QString& filename) const {
-    return iconMap.value(filename);
-}
-
-const QMap<QString, QString>& IconManager::getIconPaths() const {
-    return iconPaths;
+const QMap<QString, QString> &IconManager::getIconPaths() const {
+  return iconPaths;
 }
 
 QPixmap IconManager::recolorPixmap(const QPixmap &pixmap, const QColor &color) {
   // Recolor black pixels
-  QImage img = pixmap.toImage().convertToFormat(QImage::Format_ARGB32);
+  QImage img       = pixmap.toImage().convertToFormat(QImage::Format_ARGB32);
   QRgb targetColor = color.rgb();
-  int height = img.height();
-  int width = img.width();
+  int height       = img.height();
+  int width        = img.width();
 
   for (int y = 0; y < height; ++y) {
     QRgb *pixel = reinterpret_cast<QRgb *>(img.scanLine(y));
-    QRgb *end = pixel + width;
+    QRgb *end   = pixel + width;
     for (; pixel != end; ++pixel) {
       if (qGray(*pixel) == 0) {
         *pixel = (targetColor & 0x00FFFFFF) | (qAlpha(*pixel) << 24);
--- main.cpp
+++ main.cpp
@@ -9,64 +9,64 @@
 #include <QDebug>
 #include "iconManager.h"
 
-void printIconMap(const IconManager& iconManager) {
-    qDebug() << "Printing iconMap: ";
-    for (auto it = iconManager.getIconMap().constBegin();
-        it != iconManager.getIconMap().constEnd(); ++it) {
-        const QString& iconName = it.key();
-        const QString& iconPath = iconManager.getIconPaths().value(iconName);
-        qDebug() << "Icon Name: " << iconName;
-        qDebug() << "Icon Path: " << iconPath;
-    }
+void printIconMap(const IconManager &iconManager) {
+  qDebug() << "Printing iconMap: ";
+  for (auto it = iconManager.getIconMap().constBegin();
+       it != iconManager.getIconMap().constEnd(); ++it) {
+    const QString &iconName = it.key();
+    const QString &iconPath = iconManager.getIconPaths().value(iconName);
+    qDebug() << "Icon Name: " << iconName;
+    qDebug() << "Icon Path: " << iconPath;
+  }
 }
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QMainWindow w;
+  QApplication a(argc, argv);
+  QMainWindow w;
 
-    // load icons into the map
-    IconManager iconManager;
-    iconManager.loadIconsFromResource(":/icons");
+  // load icons into the map
+  IconManager iconManager;
+  iconManager.loadIconsFromResource(":/icons");
 
-    // print contents of iconMap to console
-    printIconMap(iconManager);
+  // print contents of iconMap to console
+  printIconMap(iconManager);
 
-    // create settings
-    QSettings settings("settings.ini", QSettings::IniFormat);
+  // create settings
+  QSettings settings("settings.ini", QSettings::IniFormat);
 
-    // central widget layout
-    QWidget *centralWidget = new QWidget;
-    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+  // central widget layout
+  QWidget *centralWidget = new QWidget;
+  QVBoxLayout *layout    = new QVBoxLayout(centralWidget);
 
-    // checkbox for toggling the theme
-    QCheckBox *checkBox = new QCheckBox("Toggle", centralWidget);
+  // checkbox for toggling the theme
+  QCheckBox *checkBox = new QCheckBox("Toggle", centralWidget);
 
-    // load settings
-    checkBox->setChecked(settings.value("checkBoxStatus", false).toBool());
+  // load settings
+  checkBox->setChecked(settings.value("checkBoxStatus", false).toBool());
 
-    // connect stateChanged signal of checkBox to a lambda function that writes
-    // the status into settings
-    QObject::connect(checkBox, &QCheckBox::stateChanged,
-                    [checkBox, &settings](int state) {
-                    settings.setValue("checkBoxStatus",
-                    checkBox->isChecked());
-    });
+  // connect stateChanged signal of checkBox to a lambda function that writes
+  // the status into settings
+  QObject::connect(checkBox, &QCheckBox::stateChanged,
+                   [checkBox, &settings](int state) {
+                     settings.setValue("checkBoxStatus", checkBox->isChecked());
+                   });
 
-    // create a toolButton containing an icon for testing
-    QToolButton toolButton;
-    QIcon testIcon = iconManager.getIcon("brush");
-    QPixmap testPm = iconManager.recolorPixmap(testIcon.pixmap(QSize(32, 32), QIcon::Normal), QColor(Qt::red));
-    QIcon testRecolorIcon;
-    testRecolorIcon.addPixmap(testPm, QIcon::Normal);
-    toolButton.setIcon(testRecolorIcon);
-    toolButton.setIconSize(testRecolorIcon.actualSize(QSize(32, 32)));
+  // create a toolButton containing an icon for testing
+  QToolButton toolButton;
+  QIcon testIcon = iconManager.getIcon("brush");
+  QPixmap testPm = iconManager.recolorPixmap(
+      testIcon.pixmap(QSize(32, 32), QIcon::Normal), QColor(Qt::red));
+  QIcon testRecolorIcon;
+  testRecolorIcon.addPixmap(testPm, QIcon::Normal);
+  toolButton.setIcon(testRecolorIcon);
+  toolButton.setIconSize(testRecolorIcon.actualSize(QSize(32, 32)));
 
-    // build the layout
-    layout->addWidget(checkBox);
-    layout->addWidget(&toolButton);
+  // build the layout
+  layout->addWidget(checkBox);
+  layout->addWidget(&toolButton);
 
-    // build the window
-    w.setCentralWidget(centralWidget);
-    w.show();
-    return a.exec();
+  // build the window
+  w.setCentralWidget(centralWidget);
+  w.show();
+  return a.exec();
 }
